2e070165d4fa0fd28637b8a9876e49735c2fb6bb
ckey3 <coenkey3@gmail.com>
2022-11-18 13:49:18 -0600

    change

---
+++ 50steps.py
@@ -0,0 +1,23 @@
+from cmath import pi
+import numpy as npx
+import time
+from ezGraph import *
+
+dx = 0.01
+x=0
+y=4
+
+# Graph
+graph = ezGraph(xmin=0, xmax=100, xLabel="Time (s)", yLabel="Height (cm)")
+graph.add(0,y) #add
+
+
+for i in range(50):
+    slope = x/5
+    x = x +dx
+    dy = slope * dx
+    y = y + dy
+    print(x,y)
+    graph.add(x, y)
+
+graph.keepOpen() 
